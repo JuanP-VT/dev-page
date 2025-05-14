1fbae1cb7f14341be76b20a1a9ed1dd42b0b0c92
Pablo <cv.juanp@gmail.com>
2025-05-13 17:57:29 -0700

    UPD; global.css & ThemeChanger component style

--- src/components/ui/buttons/ThemeChanger .tsx
+++ src/components/ui/buttons/ThemeChanger .tsx
@@ -29,18 +29,18 @@ export const ThemeChanger = () => {
     <Button
       onClick={handleThemeSwitch}
       className={clsx(
-        'flex gap-5 w-20 rounded-4xl relative bg-gradient-to-r from-red-200 via-orange-300 to-lime-300 dark:from-purple-800 dark:via-indigo-700 dark:to-green-600'
+        'flex gap-5 w-20 rounded-4xl relative bg-gradient-to-r from-red-200 via-orange-300 to-lime-300 dark:from-purple-800 dark:via-indigo-700 dark:to-green-600',
       )}
     >
       <LuMoonStar className="opacity-0 dark:opacity-100 transition-all duration-200 text-black/50" />
-      <MdOutlineWbSunny className="dark:opacity-0 transition-all duration-200" />
+      <MdOutlineWbSunny className="dark:opacity-0 transition-all duration-200 text-white/90" />
       <span
         className={clsx(
-          'w-8 h-8  rounded-full absolute transition-all left-1 duration-500 shadow-lg',
+          'w-7 h-7  rounded-full absolute transition-all left-1 duration-500 shadow-lg',
           {
             'translate-x-10 bg-white/50': theme === 'dark',
             'bg-white/50': theme === 'light',
-          }
+          },
         )}
       ></span>
     </Button>
