3803940263a0075c28c0fe3a41745ad64ffeef2f
Pablo <cv.juanp@gmail.com>
2025-05-15 18:27:21 -0700

    ADD; terminal component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import MobileDrawer from '@/components/composed/dialogs/MobileDrawer';
 import NavBar from '@/components/composed/NavBar';
+import Terminal from '@/components/composed/Terminal';
 
 export default function Page() {
   return (
@@ -8,6 +9,7 @@ export default function Page() {
       <MobileDrawer />
       <div className="px-4 py-8">
         <NavBar />
+        <Terminal />
       </div>
     </div>
   );
