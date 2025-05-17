7fd394fc732968af3744b82154e31eb8c17276fe
Pablo <cv.juanp@gmail.com>
2025-05-16 18:28:00 -0700

    UPD; change import statements to use 'type' for React and ui-types in button, select, and ui-store components

--- src/store/ui/ui-store.ts
+++ src/store/ui/ui-store.ts
@@ -1,21 +1,21 @@
 import { immer } from "zustand/middleware/immer";
 import createSelectors from "../selectors";
-import { State, Actions } from "./ui-types";
+import type { State, Actions } from "./ui-types";
 import { create } from "zustand";
 
 const initialState: State = {
-	isDrawerOpen: false,
+  isDrawerOpen: false,
 };
 
 const uiStore = create<State & Actions>()(
-	immer((set) => ({
-		...initialState,
+  immer((set) => ({
+    ...initialState,
 
-		setIsDrawerOpen: (isOpen: boolean) =>
-			set((state) => {
-				state.isDrawerOpen = isOpen;
-			}),
-	})),
+    setIsDrawerOpen: (isOpen: boolean) =>
+      set((state) => {
+        state.isDrawerOpen = isOpen;
+      }),
+  }))
 );
 
 export const useUiStore = createSelectors(uiStore);
