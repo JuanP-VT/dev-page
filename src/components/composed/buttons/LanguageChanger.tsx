1c9910bcdfc96471b1e82ed7a8a747aa2ab49a60
Pablo <cv.juanp@gmail.com>
2025-05-16 17:46:39 -0700

    chore: update tw-animate-css to version 1.3.0; refactor global CSS variables for improved theming; enhance LanguageChanger component to close drawer on language selection

--- src/components/composed/buttons/LanguageChanger.tsx
+++ src/components/composed/buttons/LanguageChanger.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { useUiStore } from '@/store/ui/ui-store';
 import { useTranslations } from 'next-intl';
 import { useRouter } from 'next/navigation';
 import { usePathname } from 'next/navigation';
@@ -9,16 +10,18 @@ export default function LanguageChanger() {
   const t = useTranslations('lang');
   const pathname = usePathname().split('/')[1];
 
+  const setIsOpen = useUiStore.use.setIsDrawerOpen();
+
   return (
     <Select onValueChange={(val) => router.push(val)} value={pathname}>
       <SelectTrigger size="sm" className="cursor-pointer text-gray-600 dark:text-gray-300">
         <SelectValue placeholder={t('language')} />
       </SelectTrigger>
-      <SelectContent className="text-gray-600 dark:bg-gray-800/30 dark:text-gray-300">
-        <SelectItem className="cursor-pointer" value="es">
+      <SelectContent className="text-gray-600 dark:bg-gray-800 dark:text-gray-300">
+        <SelectItem className="cursor-pointer" value="es" onClick={() => setIsOpen(false)}>
           {t('spanish')}
         </SelectItem>
-        <SelectItem className="cursor-pointer" value="en">
+        <SelectItem className="cursor-pointer" value="en" onClick={() => setIsOpen(false)}>
           {t('english')}
         </SelectItem>
       </SelectContent>
