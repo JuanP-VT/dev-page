f01f236f40a3d54f9b8e5b79e32549b60eaeb726
Pablo <cv.juanp@gmail.com>
2025-05-28 12:40:14 -0700

    ADD: create ThemeImage component for responsive theming and image rendering

---
+++ src/app/[locale]/cases/components/ThemeImage.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+type Props = {
+	imgSrc: string;
+	imgSrcDark?: string;
+};
+
+export default function ThemeImage({ imgSrc, imgSrcDark }: Props) {
+	const { theme = "dark" } = useTheme();
+	const [hasMounted, setHasMounted] = useState(false);
+	useEffect(() => {
+		setHasMounted(true);
+	}, []);
+
+	if (!hasMounted) {
+		return null;
+	} // avoid rendering during SSR
+
+	return (
+		<Image
+			src={theme === "light" ? imgSrc : imgSrcDark ? imgSrcDark : imgSrc}
+			alt="Onsite 2 Screenshot"
+			width={800}
+			height={400}
+			className="w-full rounded-lg border shadow-lg"
+		/>
+	);
+}
