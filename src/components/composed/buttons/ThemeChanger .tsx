d6d1c7cd912a51ec7e50960738c6587cda329c63
Pablo <cv.juanp@gmail.com>
2025-05-30 09:50:31 -0700

    UPDATE: adjust ThemeChanger component button height and icon size for improved appearance

--- src/components/composed/buttons/ThemeChanger .tsx
+++ src/components/composed/buttons/ThemeChanger .tsx
@@ -30,14 +30,14 @@ export const ThemeChanger = () => {
 			size="sm"
 			onClick={handleThemeSwitch}
 			className={clsx(
-				"relative  flex border-1 dark:border-slate-700 w-20 gap-7  rounded-xl bg-gradient-to-r from-red-200 via-orange-300 to-lime-300 dark:from-purple-700 dark:via-indigo-600 dark:to-green-500",
+				"relative  flex border-1 h-[32px] dark:border-slate-700 w-20 gap-7  rounded-xl bg-gradient-to-r from-red-200 via-orange-300 to-lime-300 dark:from-purple-700 dark:via-indigo-600 dark:to-green-500",
 			)}
 		>
 			<LuMoonStar className="text-white opacity-0 transition-all duration-200 dark:opacity-100 dark:animate-pulse" />
 			<MdOutlineWbSunny className="text-white transition-all duration-200 animate-pulse dark:opacity-0 dark:animate-none" />
 			<span
 				className={clsx(
-					"absolute left-1 h-6 w-6 rounded-xl shadow-lg transition-all duration-500 border border-zinc-300/20 dark:border-zinc-300/50 ",
+					"absolute left-1 h-[22px] w-[22px] rounded-xl shadow-lg transition-all duration-500 border border-zinc-300/20 dark:border-zinc-300/40 ",
 					{
 						"translate-x-12 bg-white/50": theme === "dark",
 						"bg-white/50": theme === "light",
