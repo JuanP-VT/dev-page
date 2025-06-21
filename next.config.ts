0eef1449c5ed48f1fece2faa8c90c2c5c709c9ca
Pablo <cv.juanp@gmail.com>
2025-06-21 10:27:50 -0700

    UPDATE: integrate lazy load SweetAlert2 for project live link confirmation and add bundle analyzer to configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
+// next.config.js
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+
+// 1. Import the bundle analyzer
+//$env:ANALYZE="true"; npm run build
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true', // Only enable if ANALYZE=true is set in your command
+});
+
+const nextConfig: NextConfig = {
+};
+
+const configWithBundleAnalyzer = withBundleAnalyzer(nextConfig);
 
 const withNextIntl = createNextIntlPlugin();
-export default withNextIntl(nextConfig);
+export default withNextIntl(configWithBundleAnalyzer);
