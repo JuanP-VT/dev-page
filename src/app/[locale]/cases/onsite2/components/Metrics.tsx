94911f0a7a587f723e49a8fa1947cb52e08fe983
Pablo <cv.juanp@gmail.com>
2025-06-28 12:11:39 -0700

    UPDATE: refactor Metrics component to simplify navigation logic and enhance image rendering with clsx for opacity transitions

--- src/app/[locale]/cases/onsite2/components/Metrics.tsx
+++ src/app/[locale]/cases/onsite2/components/Metrics.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import clsx from "clsx";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import React, { useMemo, useState } from "react";
@@ -11,34 +12,27 @@ export default function Metrics() {
 	const metrics = useGetOSMetrics();
 
 	const handleNext = () => {
-		if (currentIndex + 1 < metrics.length) {
-			setCurrentIndex((prev) => prev + 1);
-			return;
-		}
-		setCurrentIndex(0);
+		setCurrentIndex((prev) => (prev + 1) % metrics.length);
 	};
+
 	const handlePrev = () => {
-		if (currentIndex - 1 >= 0) {
-			setCurrentIndex((prev) => prev - 1);
-			return;
-		}
-		setCurrentIndex(metrics.length - 1);
+		setCurrentIndex((prev) => (prev - 1 + metrics.length) % metrics.length);
 	};
 
 	return (
 		<div className="flex flex-col">
 			<span>{t("description")}</span>
-			<div className="relative flex-flex-col pb-5">
+			<div className="relative flex flex-col pb-5">
 				<Button
 					aria-label="Previous"
-					className="absolute top-1/2 z-10 transition-all bg-zinc-200 hover:bg-zinc-200/90 active:bg-zic-200"
+					className="absolute top-1/2 z-10 transition-all bg-zinc-200 hover:bg-zinc-200/90 active:bg-zinc-200"
 					onClick={handlePrev}
 				>
 					<FaAngleLeft className="text-zinc-500" />
 				</Button>
 				<Button
 					aria-label="Next"
-					className="absolute right-0 top-1/2 z-10 transition-all bg-zinc-200 hover:bg-zinc-200/70 active:bg-zic-200"
+					className="absolute right-0 top-1/2 z-10 transition-all bg-zinc-200 hover:bg-zinc-200/70 active:bg-zinc-200"
 					onClick={handleNext}
 				>
 					<FaAngleRight className="text-zinc-500" />
@@ -46,27 +40,41 @@ export default function Metrics() {
 				<span className="flex p-2 text-2xl justify-center font-semibold text-transparent bg-clip-text bg-gradient-to-r to-emerald-400 dark:to-emerald-400 from-sky-500 dark:from-sky-500">
 					{metrics[currentIndex].title}
 				</span>
-				<div className="flex gap-4">
-					<span className="absolute dark:bg-zinc-700 bg-zinc-300 rounded-full px-2 py-1 text-sm">
+				<div className="flex gap-4 h-fit">
+					<span className="absolute dark:bg-zinc-700 bg-zinc-300 rounded-full px-2 py-1 text-sm z-10">
 						{t("before")}
 					</span>
-					<span className="absolute right-0 dark:bg-zinc-700 bg-zinc-300 rounded-full px-2 py-1 text-sm">
+					<span className="absolute right-0 dark:bg-zinc-700 bg-zinc-300 rounded-full px-2 py-1 text-sm z-10">
 						{t("after")}
 					</span>
-					<Image
-						src={metrics[currentIndex].old}
-						alt={metrics[currentIndex].title}
-						className="w-1/2 rounded-lg dark:border-zinc-700 border-2 border-zinc-500"
-						width={800}
-						height={400}
-					/>
-					<Image
-						src={metrics[currentIndex].new}
-						alt={metrics[currentIndex].title}
-						className="w-1/2 rounded-lg dark:border-zinc-700 border-2 border-zinc-500"
-						width={800}
-						height={400}
-					/>
+					<div className="w-1/2 rounded-lg dark:border-zinc-700 border-2 border-zinc-500 relative aspect-[2/1] min-h-[500px]">
+						{metrics.map((metric, index) => (
+							<Image
+								key={`${metric.title}-old`}
+								src={metric.old}
+								alt={`${metric.title} before`}
+								fill
+								className={clsx("absolute object-cover rounded-lg", {
+									"opacity-0": currentIndex !== index,
+									"opacity-100": currentIndex === index,
+								})}
+							/>
+						))}
+					</div>
+					<div className="w-1/2 rounded-lg dark:border-zinc-700 border-2 border-zinc-500 relative aspect-[2/1] min-h-[500px]">
+						{metrics.map((metric, index) => (
+							<Image
+								key={`${metric.title}-new`}
+								src={metric.new}
+								alt={`${metric.title} after`}
+								fill
+								className={clsx("absolute object-cover rounded-lg", {
+									"opacity-0": currentIndex !== index,
+									"opacity-100": currentIndex === index,
+								})}
+							/>
+						))}
+					</div>
 				</div>
 			</div>
 		</div>
